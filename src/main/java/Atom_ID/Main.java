00138f722556fa97bc276ea306e7e25c32511ed9
SavaStyle <sava.90@inbox.ru>
2024-03-22 11:11:53 +0300

    Initial

--- src/main/java/Atom_ID/Main.java
+++ src/main/java/Atom_ID/Main.java
@@ -2,6 +2,6 @@ package Atom_ID;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world1!");
     }
 }
