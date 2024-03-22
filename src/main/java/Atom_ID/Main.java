1ab6f0a98f9d674fc409af433e7b73a65cbd13ee
SavaStyle <sava.90@inbox.ru>
2024-03-22 11:21:34 +0300

    Initial 1.1

--- src/main/java/Atom_ID/Main.java
+++
@@ -1,7 +0,0 @@
-package Atom_ID;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world1!");
-    }
-}
---
+++ src/main/java/com/example/Atom_ID/AtomIdApplication.java
@@ -0,0 +1,13 @@
+package com.example.Atom_ID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomIdApplication.class, args);
+	}
+
+}
